9d569e63e2c20c2c858e440e9f54a3ccf9edc8b9
unknown <garrettgusty@gmail.com>
2020-09-27 10:03:15 -0600

    Moved v3 to Archive, will no longer be developing v3

---
+++ code/Archive/rx/gps_rx_v2/gps_rx_v2.ino
@@ -0,0 +1,188 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <RH_RF95.h>
+#include <LiquidCrystal.h>
+
+/* for feather32u4 */
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 7
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 868.0
+ 
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+ 
+// Blinky on receipt
+#define LED 13
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(6, 5, 9, 10, 11, 12);
+
+unsigned long time;
+
+void setup() {
+   // set up the LCD's number of columns and rows:
+  lcd.begin(20, 4);//number of columns and rows of display
+  lcd.print("Starting...");
+  lcd.setCursor(0,1);
+  lcd.print("RX Software V2");
+
+ pinMode(LED, OUTPUT);     
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+
+  Serial.begin(9600);
+  delay(100);
+    
+    // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    lcd.print("LoRa radio init failed");
+    while (1);
+  }
+  //Serial.println("LoRa radio init OK!");
+ 
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+   // Serial.println("setFrequency failed");
+    while (1);
+  }
+  //Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+ 
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+ 
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(23, false);
+
+}
+char buffer[50];
+byte sendLen;
+char timestr[8];
+int loopnumber=0;
+
+
+void loop() {
+
+
+  
+  if (rf95.available())
+  {
+    //lcd.print("Signal Aquired...")
+    uint8_t buf[50];
+    uint8_t len=sizeof(buf);
+  memset(buf, '\0', sizeof(buffer));
+    if (rf95.recv(buf, &len))
+    {
+      loopnumber ++;
+      digitalWrite(LED, HIGH);
+      //RH_RF95::printBuffer("Recieved: ", buf, len);
+      Serial.print("Message Recieved: ");
+      Serial.println((char*)buf);
+      
+      char* data= buf;
+//      complc= strchr(buf, ',');
+
+//Serial.println(buf);
+      char* lat= strtok(data, ",");
+      char* latstr=strtok(0,",");
+      char* lon=strtok(0,",");
+      char* lonstr=strtok(0,",");
+      char* alt=strtok(0,",");
+      char* spd=strtok(0,",");
+//      Serial.println((char*) lat);
+//      Serial.println((char*) latstr);
+//      Serial.println((char*) lonstr);
+//      Serial.println(alt);
+
+//Latitude
+char* latdegchar= (lat);//Pointer Char type (format DDMMMM.MMMM)
+char* latminchar= (lat+2);//Exclude first two digits from char array
+float latmin= atof(latminchar);//convert min to FLOAT
+float latdegmin= atof(latdegchar);
+float latdeg=(latdegmin-latmin)/100;//Subtract MIN from DEG
+float latdegdeci=latdeg+latmin/60;//Convert MIN to deci degree and add to deg
+Serial.print("Lat: ");
+Serial.println(latdegdeci, 5);
+Serial.print("lat: ");
+Serial.println(lat);
+Serial.print("latminchar: ");
+Serial.println(latminchar);
+Serial.print("latdegmin: ");
+Serial.println(latdegmin);
+Serial.print("latdegchar: ");
+Serial.println(latdegchar);
+
+  
+//Longitude
+char* londegchar= lon;
+char* lonminchar=(lon+4);
+float lonmin= atof(lonminchar);
+float londegmin= atof(londegchar);
+float londeg=(londegmin-lonmin)/100;
+float londegdeci=londeg+lonmin/60;
+Serial.print("lon: ");
+Serial.println(lon);
+Serial.print("lonminchar: ");
+Serial.println(lonminchar);
+Serial.print("londegmin: ");
+Serial.println(londegmin);
+Serial.print("londeg: ");
+Serial.println(londeg);
+
+Serial.print("lonmin: ");
+Serial.println(lonmin);
+
+//spd
+double Dspd= atof(spd);
+
+Serial.print("Lon: ");
+Serial.println(londegdeci, 5);
+Serial.print("Alt:");
+Serial.println(alt);
+Serial.print("Speed: ");
+Serial.println(spd);
+//Print to LCD
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(latdegdeci, 5);
+  lcd.print("N ");
+  //lcd.setCursor(0, 2);  
+  lcd.print(londegdeci, 5);
+  lcd.print("W");
+  lcd.setCursor(0,1);
+  lcd.print("ALT:");
+  lcd.print(alt);
+  lcd.print(" M");
+  lcd.setCursor(0,2);
+  lcd.print("SPD: ");
+  lcd.print(Dspd/1.944);
+  lcd.print(" M/S");
+      digitalWrite(LED, LOW);
+      delay(100);
+      // Reply
+//      digitalWrite(LED, HIGH);
+//      time=millis();
+//      dtostrf(time,8,1, timestr);
+//      sprintf(buffer, "time recieved: %s", timestr);
+//     sendLen= strlen(buffer);
+//     Serial.println(buffer);
+//     Serial.println("Replying...");
+//     rf95.send((uint8_t*)buffer, sendLen); 
+     digitalWrite(LED, LOW);
+      Serial.println(loopnumber);
+      lcd.setCursor(0,3);
+      lcd.print("RCV: ");
+      lcd.print(rf95.lastRssi(), DEC);
+      lcd.print(" dB ");
+      lcd.print(loopnumber);
+    }
+    
+  }
+
+}
---
+++ code/Archive/tx/gps_tx_v2/gps_tx_v2.cpp
@@ -0,0 +1,245 @@
+#include <Arduino.h>    
+#include <Adafruit_GPS.h>
+#include <SPI.h>
+#include <RH_RF95.h>
+#include <string.h>
+//#include <AESLib.h>
+using namespace std;
+/* for feather32u4 */
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 7
+#define LED 13
+#define VBATPIN A9
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 915.0
+
+// what's the name of the hardware serial port?
+#define GPSSerial Serial1
+
+// Connect to the GPS on the hardware port
+Adafruit_GPS GPS(&GPSSerial);
+     
+// Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
+// Set to 'true' if you want to debug and listen to the raw GPS sentences
+#define GPSECHO false
+#define INPUT_BUFFER_LIMIT (128 + 1)
+
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+uint32_t timer = millis();
+
+
+void send_data(char message [50]);
+float check_bat ();
+// uint16_t encrypt_to_ciphertext(char * msg, uint16_t msgLen, byte iv[]);
+// uint16_t decrypt_to_cleartext(byte msg[], uint16_t msgLen, byte iv[]);
+
+//AESLib aesLib;
+
+// unsigned char cleartext[INPUT_BUFFER_LIMIT] = {0}; // THIS IS INPUT BUFFER (FOR TEXT)
+// unsigned char ciphertext[2*INPUT_BUFFER_LIMIT] = {0}; // THIS IS OUTPUT BUFFER (FOR BASE64-ENCODED ENCRYPTED DATA)
+
+// AES Encryption Key (same as in node-js example)
+// byte aes_key[] = { 0x04, 0x0B, 0x04, 0x05, 0x05, 0x09, 0x04, 0x07, 0x05, 0x05, 0x05, 0x02, 0x05, 0x05, 0x01, 0x00 };
+
+// General initialization vector (same as in node-js example) (you must use your own IV's in production for full security!!!)
+// byte aes_iv[N_BLOCK] = { 0x04, 0x0B, 0x04, 0x05, 0x05, 0x09, 0x04, 0x07, 0x05, 0x05, 0x05, 0x02, 0x05, 0x05, 0x01, 0x00 };
+
+// Generate IV (once)
+// void aes_init() {
+//   aesLib.gen_iv(aes_iv);
+// }
+
+
+void setup()
+{
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+
+  Serial.begin(9600);
+  delay(100);
+  // aes_init();
+  Serial.println("Feather LoRa TX Test!");
+
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+ 
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+    char status_rf95[50] = "0, RF_95 TX INITIATED";
+  send_data(status_rf95);
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(23, false);
+  rf95.setModemConfig('Bw125Cr48Sf4096');// Bandwidth = 125 kHz BW, CodingRate 4/8, SpreadingFactor 4096  Slow+long range
+
+  //while (!Serial);  // uncomment to have the sketch wait until Serial is ready
+  
+  // connect at 115200 so we can read the GPS fast enough and echo without dropping chars
+  // also spit it out
+  Serial.begin(115200);
+  Serial.println("GPS TEST PARSED BEGIN");
+  
+     
+  // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
+  GPS.begin(9600);
+  // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
+  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  // uncomment this line to turn on only the "minimum recommended" data
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
+  // For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
+  // the parser doesn't care about other sentences at this time
+  // Set the update rate
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate
+  // For the parsing code to work nicely and have time to sort thru the data, and
+  // print it out we don't suggest using anything higher than 1 Hz
+     
+  // Request updates on antenna status, comment out to keep quiet
+  GPS.sendCommand(PGCMD_ANTENNA); // explained here: https://learn.adafruit.com/adafruit-ultimate-gps/external-antenna
+  char status_gps[50] = "0, GPS INITIATING";
+  send_data(status_gps);
+  delay(1000);
+  
+  // Ask for firmware version
+  GPSSerial.println(PMTK_Q_RELEASE);
+}
+char buffer[55];
+char latstr[12];
+char lonstr[12];
+char altstr[8];
+char spdstr[8];
+float bat_volt = 0;
+char bat_volt_str[5];
+
+
+void loop() // run over and over again
+{
+     memset(buffer, '\0', sizeof(buffer));
+     memset(latstr, '\0', sizeof(latstr));
+     memset(lonstr, '\0', sizeof(lonstr));
+     memset(altstr, '\0', sizeof(altstr));
+     memset(spdstr, '\0', sizeof(spdstr));
+
+  // read data from the GPS in the 'main loop'
+  char c = GPS.read();
+  // if you want to debug, this is a good time to do it!
+  if (GPSECHO)
+    if (c) Serial.print(c);
+  // if a sentence is received, we can check the checksum, parse it...
+  if (GPS.newNMEAreceived()) {
+    // a tricky thing here is if we print the NMEA sentence, or data
+    // we end up not listening and catching other sentences!
+    // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
+    //Serial.println(GPS.lastNMEA()); // this also sets the newNMEAreceived() flag to false
+    if (!GPS.parse(GPS.lastNMEA())) // this also sets the newNMEAreceived() flag to false
+      return; // we can fail to parse a sentence in which case we should just wait for another
+  }
+  // if millis() or timer wraps around, we'll just reset it
+  if (timer > millis()) timer = millis();
+
+  // approximately every 2 seconds or so, print out the current stats
+  if (millis() - timer > 1000) {
+    timer = millis(); // reset the timer
+
+    if (GPS.fix) {
+
+//      Serial.print("Location: ");
+//      Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
+//      Serial.print(", ");
+//      Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
+//      Serial.print("Speed (knots): "); Serial.println(GPS.speed);
+//      Serial.print("Angle: "); Serial.println(GPS.angle);
+      int mess_type = 1; // indicates valid GPS message to RX unit
+      int num_sats = GPS.satellites;
+      int fix_type = GPS.fixquality_3d;
+      bat_volt = check_bat();
+      // Serial.print("Altitude: "); Serial.println(GPS.altitude);
+      Serial.print("Satellites: "); Serial.println((int)GPS.satellites); 
+      Serial.print("Fix Quality: "); Serial.println((int)GPS.fixquality);
+      Serial.print("3D Fix: "); Serial.println((int)GPS.fixquality_3d);
+      dtostrf(GPS.latitude, 9, 4, latstr); //dtostrf is necessary b/c Arduino doesn't do floats or double in sprintf
+      dtostrf(GPS.longitude, 9, 4, lonstr);
+      dtostrf(GPS.speed, 5, 2, spdstr);
+      dtostrf(GPS.altitude, 5, 2, altstr);
+      dtostrf(bat_volt, 3, 2, bat_volt_str);
+      // dtostrf(check_bat, 4, 3, bat_volt);
+      // Serial.println(lonstr);
+     for (int transtrans = 1; transtrans < 5; transtrans = transtrans + 1)
+     {
+    
+      // byte sendLen;
+//      sprintf(buffer, "Lat: %s  Lon: %s W", latstr, lonstr);
+      sprintf(buffer, "%i, %s, %c, %s, %c, %s, %s, %s, %i, %i ", mess_type, latstr, GPS.lat, lonstr, GPS.lon, altstr, spdstr, bat_volt_str, num_sats, fix_type);
+      send_data(buffer);
+//      Serial.print("Lat: "); Serial.println(latstr);
+      // sendLen= strlen(buffer);
+      // Serial.print("Sending: "); Serial.println(buffer);
+      // rf95.send((uint8_t*)buffer, sendLen);
+      // delay(50);
+     }
+
+  
+    }
+    // else {
+    //   int mess_type = 2;
+    //   int num_sats = GPS.satellites;
+    //   sprintf(buffer, "%i, %i", mess_type, num_sats);
+    //   send_data(buffer);
+    //  }
+  }
+}
+
+void send_data(char message [55])
+{
+  digitalWrite(LED, HIGH);
+  byte sendLen;
+  // uint8_t mess_enc;
+  sendLen = strlen(message);
+  // mess_enc = encrypt_to_ciphertext(message, sendLen, aes_iv);
+  rf95.send((uint8_t*)message, sendLen);
+  Serial.print("Sending: "); Serial.println(message);
+  // Serial.print("Encrypted Message: "); Serial.println(mess_enc);
+  digitalWrite(LED, LOW);
+  delay(50);
+}
+
+float check_bat()
+{
+  float measuredvbat = analogRead(VBATPIN);
+  measuredvbat *= 2;    // we divided by 2, so multiply back
+  measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
+  measuredvbat /= 1024; // convert to voltage
+  Serial.print("VBat: " ); Serial.println(measuredvbat);
+  return measuredvbat;
+}
+
+// uint16_t encrypt_to_ciphertext(char * msg, uint16_t msgLen, byte iv[]) {
+//   Serial.println("Calling encrypt (string)...");
+//   // AESLib.get_cipher64_length(msgLen);
+//   int cipherlength = aesLib.encrypt((byte*)msg, msgLen, (char*)ciphertext, aes_key, sizeof(aes_key), iv);
+//                    // uint16_t encrypt(byte input[], uint16_t input_length, char * output, byte key[],int bits, byte my_iv[]);
+//   return cipherlength;
+// }
+
+// uint16_t decrypt_to_cleartext(byte msg[], uint16_t msgLen, byte iv[]) {
+//   Serial.print("Calling decrypt...; ");
+//   uint16_t dec_bytes = aesLib.decrypt(msg, msgLen, (char*)cleartext, aes_key, sizeof(aes_key), iv);
+//   Serial.print("Decrypted bytes: "); Serial.println(dec_bytes);
+//   return dec_bytes;
+// }
