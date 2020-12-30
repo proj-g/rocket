282cc81cffd828d94b85efa7340da8d6d16305f8
unknown <garrettgusty@gmail.com>
2020-12-29 21:42:37 -0700

    added neoGPS library

--- code/gps_transmitter/src/gps_tx_v4.cpp
+++ code/gps_transmitter/src/gps_tx_v4.cpp
@@ -1,66 +1,71 @@
-//Define serial port
+
 #include<Arduino.h>
+#include <NMEAGPS.h>
 // #include<Adafruit_GPS.h>
-// #include<TinyGPS++.h>
+#include <GPSport.h>
+#include <Streamers.h>
 #define GPSSerial Serial1
 
-const byte numChars = 100;
-char receivedChars[numChars];   // an array to store the received data
-char gps_string[100];
-boolean newData = false;
+static NMEAGPS gps;
+static gps_fix fix;
+
+
+static void doSomeWork()
+{
+  // Print all the things!
+
+  trace_all( DEBUG_PORT, gps, fix );
+
+} // doSomeWork
+
+//------------------------------------
+//  This is the main GPS parsing loop.
+
+static void GPSloop()
+{
+  while (gps.available( gpsPort )) {
+    fix = gps.read();
+    doSomeWork();
+  }
+
+} // GPSloop
+
+
+
+// const byte numChars = 100;
+// char receivedChars[numChars];   // an array to store the received data
+// char gps_string[100];
+// boolean newData = false;
+
+char buffer[64];
+char time[8];
+char date[8];
+char lon[12];
+char lat[12];
+char spd[8];
+char alt[8];
+char sat[2];
 
 void setup(){
-    while(!Serial);
-    Serial.begin(115200);
-    GPSSerial.begin(9600);
+    // while(!Serial);
+    // Serial.begin(115200);
+    // GPSSerial.begin(9600);
     // GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_GGAONLY);
-    
+   DEBUG_PORT.begin(9600);
+  while (!DEBUG_PORT)
+    ;
+
+  DEBUG_PORT.print( F("NMEAsimple started\n") );
+ 
+
+  gpsPort.begin( 9600 );
 }
 
-void loop(){
-    // Serial.println("Point 1");
-    void recvWithEndMarker();
-    void showNewData();
-    // Serial.println("Point 2");
-    if (Serial.available()) {
-        char c = Serial.read();
-        GPSSerial.write(c);
-    }
-    // Serial.println("Point 3");
-    if (GPSSerial.available()){
-        char c =GPSSerial.read();
-        Serial.write(c);
-    }
+//--------------------------
+// }
+
+void loop()
+{
+  GPSloop();
 }
 
-void recvWithEndMarker() {
-    static byte ndx = 0;
-    char endMarker = '\n';
-    char rc;
-    Serial.println("recvWithEndMarker Has Run");
-   
-    while (GPSSerial.available() > 0 && newData == false) {
-        rc = GPSSerial.read();
-
-        if (rc != endMarker) {
-            receivedChars[ndx] = rc;
-            ndx++;
-            if (ndx >= numChars) {
-                ndx = numChars - 1;
-            }
-        }
-        else {
-            receivedChars[ndx] = '\0'; // terminate the string
-            ndx = 0;
-            newData = true;
-        }
-    }
-}
-
-void showNewData() {
-    if (newData == true) {
-        Serial.print("This just in ... ");
-        Serial.println(receivedChars);
-        newData = false;
-    }
-}
