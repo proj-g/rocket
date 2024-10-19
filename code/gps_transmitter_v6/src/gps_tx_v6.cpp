0958b2425ae7cfa916ea1df25a2b05b90faca247
proj-g <garrettgusty@gmail.com>
2024-10-18 18:07:39 -0600

    troubleshooting in progress, fixed no serial issue

--- code/gps_transmitter_v6/src/gps_tx_v6.cpp
+++ code/gps_transmitter_v6/src/gps_tx_v6.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <Adafruit_GPS.h>
 #include <RH_RF95.h>
-#include <Adafruit_SleepyDog.h>
+// #include <Adafruit_SleepyDog.h>
 
 #define GPSSerial Serial1
 #define LED 13
@@ -16,14 +16,14 @@ RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
 char buffer[128];
 float txFreq = 903.0;
-unsigned long heartBeatTime = 3000; // Heartbeat interval in milliseconds
-unsigned long lastHeartbeat = 0;
+// unsigned long heartBeatTime = 3000; // Heartbeat interval in milliseconds
+// unsigned long lastHeartbeat = 0;
 bool gpsFix = false;
-void gpsInterrupt();
+// void gpsInterrupt();
 void send_data(const char *message);
 
 void setup() {
-  pinMode(LED, OUTPUT);
+  // pinMode(LED, OUTPUT);
   Serial.begin(115200);
   GPS.begin(9600);
   GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate
@@ -40,48 +40,51 @@ void setup() {
 
   Serial.println("LORA Module Initiated");
 
-  Watchdog.enable(5000); // Enable watchdog with a 5-second timeout
+  // Watchdog.enable(5000); // Enable watchdog with a 5-second timeout
 
-  attachInterrupt(digitalPinToInterrupt(GPSSerial.available()), gpsInterrupt, RISING);
+  // attachInterrupt(digitalPinToInterrupt(GPSSerial.available()), gpsInterrupt, RISING);
 }
 
 void loop() {
+  digitalWrite(LED, LOW);
   if (GPS.newNMEAreceived()) {
+    digitalWrite(LED,HIGH);
+    Serial.println("New NMEA");
     if (GPS.parse(GPS.lastNMEA())) {
       gpsFix = GPS.fix;
       if (gpsFix) {
         snprintf(buffer, sizeof(buffer), "Time: %lu, Lat: %f, Lon: %f, Alt: %f, Speed: %f",
                  GPS.seconds, GPS.latitudeDegrees, GPS.longitudeDegrees, GPS.altitude, GPS.speed);
-        send_data(buffer);
+        // send_data(buffer);
       }
     }
-  }
+  digitalWrite(LED,LOW);}
+// Serial.println("Loopdeedoo! ");
+  // unsigned long currentMillis = millis();
+  // if (!gpsFix && (currentMillis - lastHeartbeat >= heartBeatTime)) {
+  //   lastHeartbeat = currentMillis;
+  //   snprintf(buffer, sizeof(buffer), "Heartbeat: No GPS Fix");
+  //   send_data(buffer);
+  // }
 
-  unsigned long currentMillis = millis();
-  if (!gpsFix && (currentMillis - lastHeartbeat >= heartBeatTime)) {
-    lastHeartbeat = currentMillis;
-    snprintf(buffer, sizeof(buffer), "Heartbeat: No GPS Fix");
-    send_data(buffer);
-  }
-
-  Watchdog.reset(); // Reset the watchdog timer
+  // Watchdog.reset(); // Reset the watchdog timer
 }
 
-void gpsInterrupt() {
-  // Indicate that new data is available from GPS
-  if (GPSSerial.available()) {
-    GPS.read();
-  }
-}
+// void gpsInterrupt() {
+//   // Indicate that new data is available from GPS
+//   if (GPSSerial.available()) {
+//     GPS.read();
+//   }
+// }
 
-void send_data(const char *message) {
-  digitalWrite(LED, HIGH);
-  rf95.send((uint8_t *)message, strlen(message));
-  Serial.print("Sending: ");
-  Serial.println(message);
-  digitalWrite(LED, LOW);
-  delay(50); // Small delay to ensure message is sent
-}
+// void send_data(const char *message) {
+//   digitalWrite(LED, HIGH);
+//   rf95.send((uint8_t *)message, strlen(message));
+//   Serial.print("Sending: ");
+//   Serial.println(message);
+//   digitalWrite(LED, LOW);
+//   delay(50); // Small delay to ensure message is sent
+// }
 
 float check_bat() {
   float measuredvbat = analogRead(VBATPIN);
